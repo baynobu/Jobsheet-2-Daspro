151f386909a509d47a5b1caef287a10957eb01f8
alfredaaa <mahezzeham@gmail.com>
2024-09-04 16:19:53 +0700

    bio freda

--- BiodataMahasiswa.java
+++ BiodataMahasiswa.java
@@ -2,5 +2,6 @@ public class BiodataMahasiswa {
 
     public static void main(String[] args) {
         System.out.println("Biodata Mahasiswa: ");
+        System.out.println("Nama Saya Alfreda Dhaifullah Mahezwara, Kelas 1A, NIM 244107020219");
     }
 }
