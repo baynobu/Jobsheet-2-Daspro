302dfb6a28d78d619a0540ddab60d5370d1f7f5e
nuril <mnurilhuda2005@gmail.com>
2024-09-04 16:13:03 +0700

    bio nuril

--- BiodataMahasiswa.java
+++ BiodataMahasiswa.java
@@ -2,5 +2,6 @@ public class BiodataMahasiswa {
 
     public static void main(String[] args) {
         System.out.println("Biodata Mahasiswa: ");
+        System.out.println("Nama Saya Muhammad Nuril Huda, Kelas 1A, NIM 244107020004");
     }
 }
